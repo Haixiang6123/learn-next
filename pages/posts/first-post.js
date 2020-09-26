eb6d66bc871da7e576bd1c0281904da578836236
Haixiang <haixiang6123@gmail.com>
2020-09-26 11:02:22 +0800

    feat: 快速导航

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,10 @@
 import React from "react"
+import Link from "next/link"
 
 export default function X() {
   return (
-    <div>First Post</div>
+    <div>
+      <Link href="/">回到首页</Link>
+    </div>
   )
 }
