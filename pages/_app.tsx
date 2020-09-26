7780c90e707fe8dc32eb957e8d01e08d22997ad6
Haixiang <haixiang6123@gmail.com>
2020-09-26 18:10:09 +0800

    feat: 解决 ts 错误

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import React from "react"
+import React, {FC} from 'react'
 import Head from "next/head"
 import 'styles/globals.scss'
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: {Component: FC, pageProps: any}) => {
   return (
     <div>
       <Head>
