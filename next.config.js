d9004e5741b8f20df2d4a0981dadb969a114149d
Haixiang <haixiang6123@gmail.com>
2020-09-26 12:05:53 +0800

    feat: 使用 next-images

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
-module.exports = {
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.(jpg|jpeg|gif|png|svg)$/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[name].[contenthash].[ext]',
-          outputPath: 'static',
-          publicPath: '_next/static',
-        }
-      }]
-    })
+const withImages = require('next-images')
 
+module.exports = withImages({
+  webpack: (config, options) => {
     return config
   }
-}
+})
